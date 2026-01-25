19af7a93ed51d4fda9858294646d5ba5730be5d6
Judy He <yanqijudy@gmail.com>
2026-01-25 17:23:13 -0500

    updated django domain settings

--- src/django-backend/backend/settings.py
+++ src/django-backend/backend/settings.py
@@ -94,6 +94,8 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://vcm-51984.vm.duke.edu",
     "https://vcm-51984.vm.duke.edu",
+    "http://localhost",
+    "http://127.0.0.1",
 ]
 
 # Database

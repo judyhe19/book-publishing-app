11139ed6c3dea4396e0d5e296e98c384725b296f
Ryan_Teachman <rlt42@duke.edu>
2026-02-23 10:36:39 -0500

    push

--- src/django-backend/bookapp/views/author.py
+++ src/django-backend/bookapp/views/author.py
@@ -59,8 +59,8 @@ class AuthorViewSet(ModelViewSet):
 
             # --- annotations for table columns ---
             qs = qs.annotate(
-                authored_books_count=Count("authorbook_set__book", distinct=True),
-
+                # authored_books_count=Count("authorbook_set__book", distinct=True),
+                authored_books_count = 1
                 total_author_royalty=Coalesce(
                     Sum("sales_records__royalty_amount"),
                     Value(0),
